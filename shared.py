f111597bdc9a81098d738947ea221fb2f7d7b0e1
novica <nnovica@gmail.com>
2024-05-03 08:48:54 +0200

    read the pin from connect

--- shared.py
+++ shared.py
@@ -1,6 +1,10 @@
-from pathlib import Path
+#from pathlib import Path
 
 import pandas as pd
+from pins import board_connect
 
-app_dir = Path(__file__).parent
-df = pd.read_csv(app_dir / "penguins.csv")
+#app_dir = Path(__file__).parent
+
+board = board_connect()
+
+df = board.pin_read("vi2451/penguins_limsshiny")
