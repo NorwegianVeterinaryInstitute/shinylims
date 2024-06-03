cadce96b20ee31a8439d48a8fec8c7e1e9098654
Magnus Leithaug <mleithaug@gmail.com>
2024-06-03 18:45:41 +0200

    Transforming limsids to html links to clarity step

--- data_utils.py
+++ data_utils.py
@@ -3,9 +3,26 @@ from pins import board_connect
 from shiny import ui
 import numpy as np
 
+
+def transform_to_html(limsid):
+    if pd.isna(limsid) or limsid == '':
+        return limsid
+    ids = limsid.split(',')
+    html_links = []
+    for id in ids:
+        parts = id.split('-')
+        if len(parts) == 2:
+            html_link = f'<a href="https://nvi-prod.claritylims.com/clarity/work-complete/{parts[1]}" target="_blank">{id}</a>'
+            html_links.append(html_link)
+        else:
+            # Handle incorrectly formatted id
+            html_links.append(id)
+    return ', '.join(html_links)
+
+
 def fetch_pinned_data(pin_name):
     
-    board = board_connect()
+    board = board_connect(server_url="https://connect.posit.vetinst.no/", api_key="NCU44S5HcyA64HIVn5o7fbIKQR5a1yds")
     df = board.pin_read(pin_name)
     if 'Open Date' in df.columns:
         df['Open Date'] = pd.to_datetime(df['Open Date'])
@@ -19,5 +36,21 @@ def fetch_pinned_data(pin_name):
 
     # Find created date
     meta_created = board.pin_meta(pin_name).created
+
+
+    # Add html link for limsids
+
+    if "seq_limsid" in df.columns:
+        df['seq_limsid'] = df['seq_limsid'].apply(transform_to_html)
+
+    if "nd_limsid" in df.columns:
+        df['nd_limsid'] = df['nd_limsid'].apply(transform_to_html)
+
+    if "qubit_limsid" in df.columns:
+        df['qubit_limsid'] = df['qubit_limsid'].apply(transform_to_html)
+    
+    if "prep_limsid" in df.columns:
+        df['prep_limsid'] = df['prep_limsid'].apply(transform_to_html)
+
 
     return df, meta_created
