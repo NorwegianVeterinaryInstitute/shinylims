2e4ecc86b25d0b76e9d1afa6f17e51a6f83fe91d
Magnus Leithaug <Magnus.Leithaug@vetinst.no>
2026-03-05 14:22:01 +0100

    turn off full access

--- src/shinylims/security.py
+++ src/shinylims/security.py
@@ -13,7 +13,7 @@ CONNECT_ALLOWED_GROUP = "116-Molekylærbiologi"
 CONNECT_ALLOWED_USERS: set[str] = set() #example {"vi2172", "other_user_id"}
 
 # Local authorization config (code-defined)
-LOCAL_DEV_ALLOW_ALL = True
+LOCAL_DEV_ALLOW_ALL = False
 
 
 def is_running_on_connect() -> bool:
