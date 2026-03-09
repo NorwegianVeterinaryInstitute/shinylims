bb6a3c076939fe118a7538d94180457a9130d335
Magnus Leithaug <Magnus.Leithaug@vetinst.no>
2026-03-09 15:08:38 +0100

    fixing favicon see issue #32

--- src/shinylims/app.py
+++ src/shinylims/app.py
@@ -64,6 +64,24 @@ app_ui = ui.page_navbar(
             name="viewport",
             content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
         ),
+        ui.tags.link(
+            rel="icon",
+            type="image/png",
+            sizes="32x32",
+            href="images/favicon/favicon-32x32.png",
+        ),
+        ui.tags.link(
+            rel="icon",
+            type="image/png",
+            sizes="16x16",
+            href="images/favicon/favicon-16x16.png",
+        ),
+        ui.tags.link(
+            rel="icon",
+            type="image/png",
+            sizes="96x96",
+            href="images/favicon/favicon-96x96.png",
+        ),
         # Include CSS file
         ui.include_css(css_path),
         ui.tags.script(
